8b0bfc911b3647dfd33dcb6d33a71801f9629839
hyx <hyx@itcast.cn>
2024-12-06 11:05:20 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -104,4 +104,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(pages, records);
     }
 
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+
+        employeeMapper.update(employee);
+    }
+
 }
